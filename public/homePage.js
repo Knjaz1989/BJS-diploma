67f4ab43c17e3d4e12b18b8f11296e8181451d66
Igor Sverchkov <knjaz1989@gmail.com>
2023-08-23 20:57:31 +0300

    get currency fixed

--- public/homePage.js
+++ public/homePage.js
@@ -8,3 +8,28 @@ logoutButton.action = () => {
 		}
 	);
 }
+
+ApiConnector.current(
+	response => {
+		if (response.success) {
+			ProfileWidget.showProfile(response.data)
+		}
+	}
+);
+
+let ratesBoard = new RatesBoard();
+
+let getCurrency = () => {
+	ApiConnector.getStocks(
+		response => {
+      if (response.success){
+        ratesBoard.clearTable();
+        ratesBoard.fillTable(response.data);
+      }
+		}
+	);
+};
+
+
+getCurrency();
+setInterval(getCurrency, 60000);
